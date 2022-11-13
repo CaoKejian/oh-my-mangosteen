684655cc566d950f0ecb38b549f11ff90236118b
曹珂俭 <1849201815@qq.com>
2022-11-13 22:49:47 +0800

    引入Vue

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { defineComponent,ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
 
 
 
 export const App = defineComponent({
     setup(){
-        const refCount = ref(0)
-        const onClick=()=>{
-            refCount.value += 1
-        }
-        return ()=>(<>
-        <div>{refCount.value}</div>
-        <div><button onClick={onClick}>+1</button> </div>
-        </>)
+        return ()=><>
+        <header>导航
+            <ul>
+                <li>
+                    <router-link to="/">Foo</router-link>
+                </li>
+                <li>
+                    <router-link to="/about">Bar</router-link>
+                </li>
+            </ul>
+        </header>
+            <div>
+                <RouterView></RouterView>
+            </div>
+        </>
+        
     }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import { App } from './App'
+import { Bar } from './views/Bar'
+import { Foo } from './views/Foo'
+import {createRouter,createWebHashHistory} from 'vue-router'
 
-createApp(App).mount('#app')
+// 创建路由
+const routes = [
+    {path:'/',component:Foo},
+    {path:'/about',component:Bar},
+]
+// 创建router hash模式
+const router =createRouter({
+    history:createWebHashHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
+// createApp(App).mount('#app')
