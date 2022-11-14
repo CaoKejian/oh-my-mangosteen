8798a17175e922c8c23ee6682c671a313ddf92e2
曹珂俭 <1849201815@qq.com>
2022-11-14 14:53:05 +0800

    创建欢迎界面的页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,22 @@
+import { RouteRecordRaw } from 'vue-router'
+import { First } from '../components/Welcome/first'
+import { Forth } from '../components/Welcome/forth'
+import { Second } from '../components/Welcome/second'
+import { Third } from '../components/Welcome/third'
 import { Bar } from '../views/Bar'
 import { Foo } from '../views/Foo'
-export const routes = [
+import { Welcome } from '../views/Welcome'
+export const routes:RouteRecordRaw[] = [
     {path:'/',component:Foo},
     {path:'/about',component:Bar},
+    {
+      path:'/welcome',
+      component:Welcome,
+      children:[
+        {path:'1',component:First,},
+        {path:'2',component:Second,},
+        {path:'3',component:Third,},
+        {path:'4',component:Forth,}
+      ],
+    }
 ]
---
+++ src/views/Welcome.tsx
@@ -0,0 +1,8 @@
+import { defineComponent,ref } from "vue";
+import { RouterView } from "vue-router";
+
+export const Welcome= defineComponent({
+    setup(){
+        return ()=>(<div><RouterView/></div>)
+    }
+})
