7974bfba54278bc6846f44b7a7840eb3eca701a6
曹珂俭 <1849201815@qq.com>
2022-11-15 15:27:32 +0800

    究极重构

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,21 +1,17 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './Welcome.module.scss';
 import pig from '../../assets/icons/pig.svg';
 import { RouterLink } from 'vue-router';
-export const First = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.pig} src={pig} />
-          <h2>会挣钱<br/>还要会省钱</h2>
-        </div>
-        <div class={s.actions}>
+import {WelcomeLayout} from './WelcomeLayout'
+export const First =() => { 
+    return<WelcomeLayout >{{
+      icon:() => <img src={pig}></img>,
+      title:() => <h2>会挣钱<br/>还会省钱</h2>,
+      buttons:() => <>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
           <RouterLink to="/welcome/2" >下一页</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
-        </div>
-      </div>
-    )
+          <RouterLink  to="/start" >跳过</RouterLink>
+      </>
+    }}
+    </WelcomeLayout>
   }
-})
+First.displayName = 'First'
--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,21 +1,20 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './Welcome.module.scss';
 import cloud from '../../assets/icons/cloud.svg';
 import { RouterLink } from 'vue-router';
-export const Forth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.cloud} src={cloud} />
-          <h2>会挣钱<br/>还要会省钱</h2>
-        </div>
-        <div class={s.actions}>
+import {WelcomeLayout} from './WelcomeLayout'
+export const Forth = ()=>{
+  return(
+    <WelcomeLayout>
+      {{
+        icon:()=> <img class={s.icon} src={cloud}></img>,
+        title:()=> <h2>每日提醒<br></br>不遗漏每一笔账单</h2>,
+        buttons:()=> <>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/start" >下一页</RouterLink>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-        </div>
-      </div>
-    )
+          <RouterLink to="/start" >进入</RouterLink>
+          <RouterLink  to="/start" class={s.fake}  >跳过</RouterLink>
+        </>
+      }}
+    </WelcomeLayout>
+  )
   }
-})
+Forth.displayName="Forth"
