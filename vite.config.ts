72364dfad59009fac7bf49d4d2f48f073cbdc136
曹珂俭 <1849201815@qq.com>
2022-12-03 19:51:38 +0800

    压缩打包

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
 // @ts-nocheck
 import { svgstore } from './src/vite_plugins/svgstore';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/oh-my-mangosteen/dist/',
   plugins: [
     vue(),
+    viteCompression(),
     vueJsx({
       transformOn: true,
       mergeProps: true
